fff223fd5845339b4e3215adf661a363c2660034
immisius <immisius@gmail.com>
2022-07-27 11:30:38 +0900

    aa

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import csv
 from random import choices
 import pandas as pd
 import datetime
-df=pd.read_csv('words.csv',encoding='utf-8')
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+import re
+
+df=pd.read_csv('words.csv',encoding='utf-8')
 gb = GridOptionsBuilder.from_dataframe(df, editable=True)
 grid = AgGrid(df, gridOptions=gb.build(), updateMode=GridUpdateMode.VALUE_CHANGED)
 
 # 修正が反映される
 st.dataframe(grid['data'])
-import re
+
 with st.sidebar.form('追加'):
     words=st.text_input('単語', '')
     type=st.radio('どのタイプですか',('知らない単語','覚えたい表現'))
